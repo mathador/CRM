89fa2896b8cab9b322f8a9c01109f75fc2394ccd
Christophe <christophe.machu@techvalley.fr>
2019-08-25 20:19:11 +0200

    video 03 02 ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  nombreLettres(CSSClass) {
+    if (CSSClass === 'parfait')
+      return true;
+  }
+
   header = 'CRM';
   titre = 'Gestion des relations clients';
   person = 'Tatum';
-  contacts =[{"first_name":"Tatum","last_name":"Vernon","email":"tvernon2@lycos.com","gender":"Female","company":"Youopia"},
-  {"first_name":"Anet","last_name":"Bellis","email":"abellis1@cnn.com","gender":"Female","company":"Oloo"},
-  {"first_name":"Pippa","last_name":"Goymer","email":"pgoymer2@ihg.com","gender":"Female","company":"Browsecat"},
-  {"first_name":"Addison","last_name":"Lawther","email":"alawther3@walmart.com","gender":"Male","company":"Yoveo"},
-  {"first_name":"Anya","last_name":"Franzman","email":"afranzman4@bravesites.com","gender":"Female","company":"Twitterbeat"}];
+  contacts = [{ "first_name": "Tatum", "last_name": "Vernon", "email": "tvernon2@lycos.com", "gender": "Female", "company": "Youopia" },
+  { "first_name": "Anet", "last_name": "Bellis", "email": "abellis1@cnn.com", "gender": "Female", "company": "Oloo" },
+  { "first_name": "Pippa", "last_name": "Goymer", "email": "pgoymer2@ihg.com", "gender": "Female", "company": "Browsecat" },
+  { "first_name": "Addison", "last_name": "Lawther", "email": "alawther3@walmart.com", "gender": "Male", "company": "Yoveo" },
+  { "first_name": "Anya", "last_name": "Franzman", "email": "afranzman4@bravesites.com", "gender": "Female", "company": "Twitterbeat" }];
 }
