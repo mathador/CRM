c2e082ac58138f2124fbb29ee9b411ee4435f3ee
Christophe <christophe.machu@techvalley.fr>
2019-08-28 10:25:25 +0200

    création du modèle

---
+++ src/app/models/Contact.ts
@@ -0,0 +1,11 @@
+export class Contact {
+    constructor(
+        public first_name: string,
+        public last_name: string,
+        //public age: number,
+        public email: string,
+        public gender: string,
+        public company: string
+        //public hobbies?: string[]
+    ) { }
+}
