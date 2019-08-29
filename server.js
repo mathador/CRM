ba9dee2afe5fa34fe26b19c4822d7db0b999014b
Christophe <christophe.machu@techvalley.fr>
2019-08-29 13:15:14 +0200

    mi se place du server, faire un build d'abord puis npm start

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var http = require('http');
+var express = require('express');
+//const util = require('util'); // pour les log detaillés
+var path = require('path');
 
-var server = http.createServer(function (req, res) {
-    console.log('************************************')
-    console.info(req);
-    res.writeHead(200);
-    res.end('Salut tout le monde !');
-    console.log('++++++++++++++++++++++++++++++++++++')
-    console.warn(res);
-});
-server.listen(1234);
+var app = express();
+var collection = [];
+var port = 1234;
+
+app.use(express.static(__dirname + '/dist/CRM'));
+
+app.get('/', function (req, res) {
+    //console.info('path: '+path.resolve();
+    //console.log(util.inspect(path, false, null, true /* enable colors */))
+    //console.log(util.inspect(path, {showHidden: false, depth: null}))
+    //res.sendFile(path.join('./src/index.html'));
+    res.sendFile(path.join(__dirname));
+})
+
+app.get('/contacts', function (req, res) {
+    console.info('contacts: ' + collection);
+    res.json(collection);
+})
+
+
+app.listen(port);
+console.log('Server up and running on http://localhost:' + port);
